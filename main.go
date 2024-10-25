c033e4ab4992433b042a418a4e009e8263827920
yassine-bouchoucha <ybou@ip-label.com>
2024-10-25 15:29:58 +0000

    imlement logger middleware

--- main.go
+++ main.go
@@ -5,9 +5,17 @@ import (
 	"net/http"
 )
 
+func logger(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println("REQ", r.Method, r.URL.Path)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
 
 	mux := http.NewServeMux()
+	muxWithLogger := logger(mux)
 
 	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello World !!"))
@@ -15,13 +23,13 @@ func main() {
 
 	mux.HandleFunc("GET /posts/{postId}", func(w http.ResponseWriter, r *http.Request) {
 		postId := r.PathValue("postId")
-		log.Println(postId)
+		log.Println("POST ID", postId)
 		w.Write([]byte("success"))
 
 	})
 
 	log.Println("server stared on port 8080")
-	if err := http.ListenAndServe(":8080", mux); err != nil {
+	if err := http.ListenAndServe(":8080", muxWithLogger); err != nil {
 		panic(err)
 	}
 
