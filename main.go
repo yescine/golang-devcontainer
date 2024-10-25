899cdc4a08b916e9a589a2a4c2b87e119a0a8954
yassine-bouchoucha <ybou@ip-label.com>
2024-10-25 15:20:38 +0000

    basic http server

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "log"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 
-	log.Println("Hello World")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello World !!"))
+	})
+
+	mux.HandleFunc("GET /posts/{postId}", func(w http.ResponseWriter, r *http.Request) {
+		postId := r.PathValue("postId")
+		log.Println(postId)
+		w.Write([]byte("success"))
+
+	})
+
+	log.Println("server stared on port 8080")
+	if err := http.ListenAndServe(":8080", mux); err != nil {
+		panic(err)
+	}
+
 }
