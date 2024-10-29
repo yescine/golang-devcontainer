83778f7b89f4e946d55751f0d1356d988335484e
yescine <bouchoucha.yassine@gmail.com>
2024-10-29 12:17:10 +0000

    encode struc to json

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ docker build -f Dockerfile.dev -t go-dev .
 docker run --name go-dev -v .:/app go-dev
 ```
 Or use VsCode devcontainer
+
+# Start
+
+```bash 
+go run main.go
+
+# other script
+cd scripts
+go run script_file.go
+
+```
--- scripts/json_parse.go
+++ scripts/json_parse.go
@@ -12,10 +12,11 @@ type Todo struct {
 	ID     int    `json:"id"`
 	Title  string `json:"title"`
 	// Completed bool   `json:"completed"`
+	hash string // private field
 }
 
 func main() {
-	url := "https://jsonplaceholder.typicode.com/todos/2"
+	url := "https://jsonplaceholder.typicode.com/todos/1"
 
 	response, err := http.Get(url)
 	if err != nil {
@@ -26,13 +27,19 @@ func main() {
 	if response.StatusCode == http.StatusOK {
 		var todoItem Todo
 		decoder := json.NewDecoder(response.Body)
-		decoder.DisallowUnknownFields()
+
+		//! decoder.DisallowUnknownFields()
 
 		if err := decoder.Decode(&todoItem); err != nil {
 			log.Fatal("Decode error:", err)
 		} else {
 			fmt.Printf("Data from API: %+v\n", todoItem)
 		}
+
+		// Encode to json
+		todo, _ := json.MarshalIndent(todoItem, "", "\t")
+		fmt.Println("\n\n", string(todo))
+
 	} else {
 		log.Fatalf("Request failed with status: %d", response.StatusCode)
 	}
